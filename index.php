256b03a15b717a295bccbf6106dd62006d4639f1
Bob Smith <overcomethestruggle@outlook.com>
2013-11-26 21:11:00 -0800

    Created index of site, and meditation timer page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Overcome the Struggle</title>
+</head>
+<body>
+	<h1>Welcome!</h1>
+	<p>Welcome to Overcome the Struggle! This site is dedicated to helping people overcome their addictions to pornography.</p>
+	<p>It is based off of the recommendations of several books, however all information and services on this site are provided without warranty. We are not responsible for the accuracy or effectiveness of the information and services. They are provided for your benefit, but should not be considered as a replacement for professional counseling or psychological help.</p>
+	<h1>Are you addicted to pornography?</h1>
+	<p>If you think you are addicted to pornography, start on the <a href = "/trail">trail to recovery.</a></p>
+</body>
+</html>
